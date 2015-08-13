e4fc83738b4bd1d75d21e728218033b988475658
Nidaoke <jnidaokefantagefacebook27@gmail.com>
2015-08-13 19:41:07 -0400

    New Ship Data
    Changed the data for the new ship including variables, bullet, etc...
    Also moved some scripts of mine into better locations.

---
+++ Assets/_Scripts/Weapons/BasicDoubleShot.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Assets._Scripts.Weapons
+{
+	public class BasicDoubleShot : Weapon
+	{
+		public override void Shoot(){
+			
+			try
+			{
+				if(!isUpgraded){
+
+					var spawnedProjectile1 = (Projectile)Instantiate(WeaponProjectile, new Vector2(gameObject.transform.position.x - 1, gameObject.transform.position.y), gameObject.transform.rotation);
+					spawnedProjectile1.Init(Damage, Speed);
+
+					var spawnedProjectile2 = (Projectile)Instantiate(WeaponProjectile, new Vector2(gameObject.transform.position.x + 1, gameObject.transform.position.y), gameObject.transform.rotation);
+					spawnedProjectile2.Init(Damage, Speed);
+				}else{
+					
+					/*var spawnedProjectile = (Projectile)Instantiate(WeaponProjectile, gameObject.transform.position, gameObject.transform.rotation);
+					spawnedProjectile.Init(UpgradedDamage, UpgradedSpeed);
+					
+					var spawnedProjectileLeft = (Projectile)Instantiate(UpgradedWeaponProjectile, gameObject.transform.position, gameObject.transform.rotation);
+					spawnedProjectileLeft.transform.Rotate(new Vector3(0, 0, -45));
+					spawnedProjectileLeft.Init(UpgradedDamage, UpgradedSpeed);
+					
+					var spawnedProjectileRight = (Projectile)Instantiate(UpgradedWeaponProjectile, gameObject.transform.position, gameObject.transform.rotation);
+					spawnedProjectileRight.transform.Rotate(new Vector3(0, 0, 45));
+					spawnedProjectileRight.Init(UpgradedDamage, UpgradedSpeed);*/
+				}
+			}
+			catch
+			{
+				Debug.Log("There was an error during creating a projectile");
+			}
+		}
+	}
+}
