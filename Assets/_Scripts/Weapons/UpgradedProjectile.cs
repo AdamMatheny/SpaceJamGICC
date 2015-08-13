4ee5f9b63e2ccf8800650480b249486601e5fbc7
Nidaoke <jnidaokefantagefacebook27@gmail.com>
2015-08-13 14:48:03 -0400

    Upgraded Bullets
    - Disabled the audio listener in the Right Camera to prevent the error.
    - Added in a bool that overrides the Shoot method inside the basic
    cannon for an upgraded weapon.
    - Put in a test upgraded weapon for the basic cannon, now shoots three
    bullets, two are angled.
    - Also added in upgraded settings (Speed, Damage, and Fire Rate) for
    upgraded weapons.
    - Added an UpgradedProjectile script specifically for editing Upgraded
    Bullets. Nothing in here yet though.
    - Changed camera sizes.

---
+++ Assets/_Scripts/Weapons/UpgradedProjectile.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Assets._Scripts.Weapons{
+	public class UpgradedProjectile : Projectile {
+
+	}
+}
--- Assets/_Scripts/Weapons/Weapon.cs
+++ Assets/_Scripts/Weapons/Weapon.cs
@@ -9,15 +9,33 @@ namespace Assets._Scripts.Weapons
         public float Speed;
         public float FireRate;
 
-        [Header("Projectyles to Shoot")]
-        public Projectile WeaponProjectile;
+		[Header("Projectyles to Shoot")]
+		public Projectile WeaponProjectile;
+
+		[Header("Upgraded Weapon Info")] // ~ Jonathan
+		public bool isUpgraded; 
+		public int UpgradedDamage;
+		public float UpgradedSpeed;
+		public float UpgradedFireRate;
+
+		[Header("Upgraded Projectile")] // ~ Jonathan
+		public Projectile UpgradedWeaponProjectile;
 
         public virtual void Shoot()
         {
             try
             {
-                var spawnedProjectile = (Projectile)Instantiate(WeaponProjectile, gameObject.transform.position, gameObject.transform.rotation);
-                spawnedProjectile.Init(Damage, Speed);
+				if(!isUpgraded){
+
+					Debug.Log("Created a basic bullet.");
+					var spawnedProjectile = (Projectile)Instantiate(WeaponProjectile, gameObject.transform.position, gameObject.transform.rotation);
+					spawnedProjectile.Init(Damage, Speed);
+				}else{
+
+					Debug.Log("Created an upgraded bullet.");
+					var spawnedProjectile = (Projectile)Instantiate(UpgradedWeaponProjectile, gameObject.transform.position, gameObject.transform.rotation);
+					spawnedProjectile.Init(Damage, Speed);
+				}
             }
             catch
             {
