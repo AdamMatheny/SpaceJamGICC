d4b993c07ea4185a81f55a4051d8a31a530d9802
Nidaoke <jnidaokefantagefacebook27@gmail.com>
2015-08-13 20:21:38 -0400

    Converted Ships' movement to Rigid Body

--- Assets/_Scripts/Player/PlayerControl.cs
+++ Assets/_Scripts/Player/PlayerControl.cs
@@ -19,19 +19,31 @@ namespace Assets._Scripts.Player
         [Header("Movement Variables")]
         [SerializeField]
         private float FlySpeed;
+		public Rigidbody2D rgbd2D;
+
+		void Start(){
+
+			rgbd2D = GetComponent<Rigidbody2D> ();
+		}
 
         void Update()
         {
 
-            if (Input.GetAxis(HorizontalAxisName) >= AxisDeadZone || Input.GetAxis(HorizontalAxisName) <= -AxisDeadZone)
-            {
-                gameObject.transform.Translate(Input.GetAxis(HorizontalAxisName) * FlySpeed * Time.deltaTime, 0, 0);
-            }
+            if (Input.GetAxis (HorizontalAxisName) >= AxisDeadZone || Input.GetAxis (HorizontalAxisName) <= -AxisDeadZone) {
+				//gameObject.transform.Translate(Input.GetAxis(HorizontalAxisName) * FlySpeed * Time.deltaTime, 0, 0);
+				rgbd2D.velocity = new Vector2 (Input.GetAxis (HorizontalAxisName) * FlySpeed * Time.deltaTime, rgbd2D.velocity.y);
+			} else {
 
-            if (Input.GetAxis(VerticalAxisName) >= AxisDeadZone || Input.GetAxis(VerticalAxisName) <= -AxisDeadZone)
-            {
-                gameObject.transform.Translate(0,Input.GetAxis(VerticalAxisName) * FlySpeed * Time.deltaTime, 0);
-            }
+				rgbd2D.velocity = new Vector2 (0, rgbd2D.velocity.y);
+			}
+
+            if (Input.GetAxis (VerticalAxisName) >= AxisDeadZone || Input.GetAxis (VerticalAxisName) <= -AxisDeadZone) {
+				//gameObject.transform.Translate(0,Input.GetAxis(VerticalAxisName) * FlySpeed * Time.deltaTime, 0);
+				rgbd2D.velocity = new Vector2 (rgbd2D.velocity.x, Input.GetAxis (VerticalAxisName) * FlySpeed * Time.deltaTime);
+			} else {
+
+				rgbd2D.velocity = new Vector2 (rgbd2D.velocity.x, 0);
+			}
 
 			//No need for this since there will be auto fire ~ Jonathan
             /*if (Input.GetKeyDown(FireButton))
