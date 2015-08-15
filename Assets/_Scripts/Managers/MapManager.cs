bd80b85ebeee50320f253be96c22367128a2d391
Wulo <thewulo@gmail.com>
2015-08-15 18:39:05 +0200

    Map Backgrounds

--- Assets/_Scripts/Managers/MapManager.cs
+++ Assets/_Scripts/Managers/MapManager.cs
@@ -1,4 +1,6 @@
 ﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Assets._Scripts.Managers
 {
@@ -6,5 +8,23 @@ namespace Assets._Scripts.Managers
     {
         public Transform PlayersTransform;
         public Transform ParticlesTransform;
+
+        [SerializeField]
+        private List<SpriteRenderer> Player1LevelBackground;
+        [SerializeField]
+        private List<SpriteRenderer> Player2LevelBackground;
+
+        public void ShowBackground(int level)
+        {
+            if (level < 1 || level > 5)
+            {
+                Debug.LogError("Tried to show Level that does not exits");
+                return;
+            }
+            Player1LevelBackground.ForEach(background => background.enabled = false);
+            Player2LevelBackground.ForEach(background => background.enabled = false);
+            Player1LevelBackground[level - 1].enabled = true;
+            Player2LevelBackground[level - 1].enabled = true;
+        }
     }
 }
