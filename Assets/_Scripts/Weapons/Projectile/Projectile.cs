9a88ba285b78e437016247baa4fbaba4c2c84e35
Nidaoke <jnidaokefantagefacebook27@gmail.com>
2015-08-14 22:10:54 -0400

    Player/Weapon Setup

---
+++ Assets/_Scripts/Weapons/Projectile/Projectile.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+//using Assets._Scripts.AI;
+//using Assets._Scripts.Managers;
+
+namespace Assets._Scripts.Weapons
+{
+	public class Projectile : MonoBehaviour
+	{
+		
+		public bool isPlayerOne;
+		
+		public Animator ProjectileAnimator;
+		
+		public int Damage;
+		public float FlySpeed;
+		
+		public void Init(int damage, float flySpeed)
+		{
+			Damage = damage;
+			FlySpeed = flySpeed;
+			
+			//ProjectileAnimator.ShowSpawnAnimaton;
+			StartCoroutine ("DieAfterTimeCoroutine");
+		}
+		
+		protected virtual void Move()
+		{
+			gameObject.transform.Translate(Vector3.up * FlySpeed * Time.deltaTime);
+		}
+
+		//Gotta get in enemies first! ~ Jonathan
+		/*protected virtual void DealDamage(Enemy enemy)
+		{
+			//ProjectileAnimator.ShowHitAnimation;
+			enemy.EnemyShipDie();
+		}*/
+		
+		void Update()
+		{
+			Move();
+			//No managers yet! ~ Jonathan
+			//gameObject.transform.parent = ObjectManager.instance.ContainerTransform;
+		}
+		
+		private IEnumerator DieAfterTimeCoroutine()
+		{
+			while (true)
+			{
+				yield return new WaitForSeconds(10);
+				Destroy(gameObject);
+			}
+		}
+	}
+}
