3c0396ea664076b4b0c574c26a271818130261cf
Wulo <thewulo@gmail.com>
2015-08-15 03:57:05 +0200

    UI Prefab, UI Manager

---
+++ Assets/_Scripts/Managers/GUIManager.cs
@@ -0,0 +1,46 @@
+﻿using Assets._Scripts.UI;
+using System.Collections.Generic;
+using System.Linq;
+using System;
+using UnityEngine;
+
+namespace Assets._Scripts.Managers
+{
+    public class GUIManager : Singleton<GUIManager>
+    {
+        [SerializeField]
+        private List<GUIScreen> GUIScreens;
+
+        private ScreenType currentScreen;
+
+        public void ShowScreen(ScreenType type)
+        {
+            HideCurrentScreen();
+            GUIScreens.First(screen => screen.ScreenType == type).Show();
+            currentScreen = type;
+        }
+
+        public void ShowScreen(int index)
+        {
+            ScreenType type = (ScreenType)index;
+            ShowScreen(type);
+        }
+
+        public void ShowScreen(string name)
+        {
+            ScreenType type = (ScreenType)Enum.Parse(typeof(ScreenType), name);
+            ShowScreen(type);
+        }
+
+        private void HideCurrentScreen()
+        {
+            if (currentScreen != ScreenType.None)
+                GUIScreens.First(screen => screen.ScreenType == currentScreen).Hide();
+        }
+
+        void Start()
+        {
+            ShowScreen(ScreenType.MainMenu);
+        }
+    }
+}
---
+++ Assets/_Scripts/UI/GUIScreen.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+namespace Assets._Scripts.UI
+{
+    public enum ScreenType {None = 0, MainMenu = 1 , PlayScreen = 2}
+
+    public class GUIScreen : MonoBehaviour
+    {
+        protected Canvas targetCanvas;
+
+        public ScreenType ScreenType;
+
+        protected virtual void Awake()
+        {
+            try
+            {
+                targetCanvas = GetComponent<Canvas>();
+            }
+            catch
+            {
+                Debug.LogError("Gameobject " + gameObject.name + " has GUI script attached but does not have Canvas component!");
+            }
+        }
+
+        public virtual void Show()
+        {
+            targetCanvas.enabled = true;
+        }
+
+        public virtual void Hide()
+        {
+            targetCanvas.enabled = false;
+        }
+    }
+}
