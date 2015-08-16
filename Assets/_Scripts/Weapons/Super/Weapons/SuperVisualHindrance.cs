5ca9640a39c60dbd687e534829d2015cb9d1967c
Nidaoke <jnidaokefantagefacebook27@gmail.com>
2015-08-16 12:17:28 -0400

    VisualHindrance

---
+++ Assets/_Scripts/Weapons/Super/Weapons/SuperVisualHindrance.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using Assets._Scripts.Player;
+using Assets._Scripts.AI;
+using Assets._Scripts.Managers;
+
+namespace Assets._Scripts.Weapons
+{
+	public class SuperVisualHindrance : Weapon
+	{
+
+		public int playerIndex;
+		
+		public GameObject[] players = new GameObject[1];
+		public GameObject activatingPlayer;
+		
+		public override void Shoot(GameObject playerToAffect){
+			
+			try
+			{
+				players = GameObject.FindGameObjectsWithTag("Player");
+				
+				foreach(GameObject player in players){
+					
+					if(player.gameObject != playerToAffect.gameObject){
+						
+						activatingPlayer = player;
+					}
+				}
+
+				playerIndex = playerToAffect.GetComponent<PlayerBase> ().mPlayerNumber;
+
+				CameraManager.instance.SetVisualHindranceForPlayer(playerIndex);
+			}
+			catch
+			{
+				Debug.Log("There was an error during creating a projectile");
+			}
+		}
+	}
+}
