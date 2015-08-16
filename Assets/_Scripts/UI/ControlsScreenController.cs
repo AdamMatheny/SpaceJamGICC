74941afb21fc64d007f453403970e1814c772644
Wulo <thewulo@gmail.com>
2015-08-16 19:57:41 +0200

    Controls Screen

---
+++ Assets/_Scripts/UI/ControlsScreenController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using Assets._Scripts.Managers;
+
+namespace Assets._Scripts.UI
+{
+    public class ControlsScreenController : GUIScreen
+    {
+        public ScreenType NextScreenToShow;
+
+        void Update()
+        {
+            if (!targetCanvas.enabled) return;
+
+            if (Input.GetKeyDown(ControlsManager.instance.Player1Controls.MegaWeaponFireButton) || Input.GetKeyDown(ControlsManager.instance.Player1Controls.MegaWeaponFireButtonGamePad))
+            {
+                GUIManager.instance.ShowScreen(NextScreenToShow);
+            }
+        }
+    }
+}
